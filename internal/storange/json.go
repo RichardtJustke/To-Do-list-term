d3a98efc0a44a2936891c15b7627ec384a145aa2
Richardt Justke <rj.justke@gmail.com>
2026-05-24 14:14:53 -0300

    json fdp

---
+++ internal/storange/json.go
@@ -0,0 +1,35 @@
+package storage
+
+import (
+	"encoding/json"
+	"os"
+
+	"todo/internal/models"
+)
+
+func SaveTasks(tasks []models.Task) error {
+	data, err := json.MarshalIndent(tasks, "", " ")
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile("data/tasks.json", data, 0o644)
+
+	return err
+}
+
+func LoadTasks() ([]models.Task, error) {
+	data, err := os.ReadFile("data/tasks.json")
+	if err != nil {
+		return nil, err
+	}
+
+	var tasks []models.Task
+
+	err = json.Unmarshal(data, &tasks)
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
